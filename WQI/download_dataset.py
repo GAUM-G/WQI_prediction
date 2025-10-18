89fbcbe3a9724459938bd3c97bbe3fb4012d05eb
Gautam Gupta <gautamgupta986047@gmail.com>
2025-10-18 13:48:01 +0530

    Delete WQI directory

--- WQI/download_dataset.py
+++
@@ -1,7 +0,0 @@
-import kagglehub
-
-# Download latest version of the dataset
-path = kagglehub.dataset_download("akkshaysr/nwmp-water-quality-data-for-indian-lakes")
-
-print("✅ Dataset downloaded successfully!")
-print("📂 Path to dataset files:", path)
